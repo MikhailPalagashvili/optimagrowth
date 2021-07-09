62de6ec190348371391227ed329af9494caf8e75
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2021-07-09 02:04:15 -0400

    Initial commit

---
+++ src/main/java/com/optimagrowth/optimagrowth/OptimagrowthApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.optimagrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimagrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimagrowthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optimagrowth/optimagrowth/OptimagrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.optimagrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimagrowthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
